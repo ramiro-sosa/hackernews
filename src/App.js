abe2cd7f7872814f15afdd7fd540626e8ac6974f
Ramiro Sosa <ramiro.sosa@gmail.com>
2019-02-03 10:04:26 -0300

    Part 1 - Introduction to React: ES6 Arrow Functions

--- src/App.js
+++ src/App.js
@@ -24,8 +24,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        {list.map(function(item) {
-          return (
+        {list.map(item => 
             <div key={item.objectID}>
               <span>
                 <a href={item.url}>{item.title}</a>
@@ -34,8 +33,7 @@ class App extends Component {
               <span>{item.num_comments}</span>
               <span>{item.points}</span>
             </div>
-          );
-        })}
+        )}
       </div>
     );
   }
