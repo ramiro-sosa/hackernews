db0e24273182d15253411f5b021470d5f2c0252e
Ramiro Sosa <ramiro.sosa@gmail.com>
2019-02-09 18:42:50 -0300

    Part 2 - Basics in React: Bindings (in render method event handler - bad practice)

--- src/App.js
+++ src/App.js
@@ -73,19 +73,13 @@ class App extends Component {
 }
 
 class ExplainBindingsComponent extends Component {
-
-  constructor() {
-    super();
-    this.onClickMe = this.onClickMe.bind(this);
-  }
-
   onClickMe() {
     console.log(this);
   }
 
   render() {
     return (
-      <button onClick={this.onClickMe} type="button">
+      <button onClick={this.onClickMe.bind(this)} type="button">
         Click Me
       </button>
     );
