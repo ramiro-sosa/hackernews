ea50e2c8543fbc427bfefba5d7b84068f5dc9741
Ramiro Sosa <ramiro.sosa@gmail.com>
2019-02-09 18:15:49 -0300

    Part 2 - Basics in React: Unidirectional Data Flow

--- src/App.js
+++ src/App.js
@@ -33,6 +33,13 @@ class App extends Component {
     this.state = {
       list
     };
+
+    this.onDismiss = this.onDismiss.bind(this);
+  }
+
+  onDismiss(id) {
+    const updatedList = this.state.list.filter(item => item.objectID !== id);
+    this.setState({ list: updatedList });
   }
 
   render() {
@@ -47,6 +54,14 @@ class App extends Component {
             <span>{item.author}</span>
             <span>{item.num_comments}</span>
             <span>{item.points}</span>
+            <span>
+              <button
+                onClick={() => this.onDismiss(item.objectID)}
+                type="button"
+              >
+                Dismiss
+              </button>
+            </span>
           </div>
         ))}
       </div>
