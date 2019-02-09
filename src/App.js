af8428778ccb8b3d3cc043f32cc6546fa25ec905
Ramiro Sosa <ramiro.sosa@gmail.com>
2019-02-09 19:24:49 -0300

    Part 2 - Basics in React: Bindings (business logic of method in constructor - bad practice)

--- src/App.js
+++ src/App.js
@@ -73,13 +73,16 @@ class App extends Component {
 }
 
 class ExplainBindingsComponent extends Component {
-  onClickMe() {
-    console.log(this);
+  constructor() {
+    super();
+    this.onClickMe = () => {
+      console.log(this);
+    };
   }
 
   render() {
     return (
-      <button onClick={this.onClickMe.bind(this)} type="button">
+      <button onClick={this.onClickMe} type="button">
         Click Me
       </button>
     );
