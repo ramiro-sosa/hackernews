8f4cd97fd02a7ae12247065cd9ab751501e81bb5
Ramiro Sosa <ramiro.sosa@gmail.com>
2019-02-16 11:00:38 -0300

    Part 2 - Basics in React: Split Up Components

--- src/App.js
+++ src/App.js
@@ -55,15 +55,31 @@ class App extends Component {
     const { searchTerm, list } = this.state;
     return (
       <div className="App">
-        <form>
-          <input
-            type="text"
-            value={searchTerm}
-            onChange={this.onSearchChange}
-          />
-        </form>
+        <Search value={searchTerm} onChange={this.onSearchChange} />
+        <Table list={list} pattern={searchTerm} onDismiss={this.onDismiss} />
+      </div>
+    );
+  }
+}
+
+class Search extends Component {
+  render() {
+    const { value, onChange } = this.props;
+    return (
+      <form>
+        <input type="text" value={value} onChange={onChange} />
+      </form>
+    );
+  }
+}
+
+class Table extends Component {
+  render() {
+    const { list, pattern, onDismiss } = this.props;
+    return (
+      <div>
         <div># Items: {listService.getNumItems(list)}</div>
-        {list.filter(isSearched(searchTerm)).map(item => {
+        {list.filter(isSearched(pattern)).map(item => {
           return (
             <div key={item.objectID}>
               <span>
@@ -73,10 +89,7 @@ class App extends Component {
               <span>{item.num_comments}</span>
               <span>{item.points}</span>
               <span>
-                <button
-                  onClick={() => this.onDismiss(item.objectID)}
-                  type="button"
-                >
+                <button onClick={() => onDismiss(item.objectID)} type="button">
                   Dismiss
                 </button>
               </span>
