2002c4898a545ebfdc7718d6f220097fcc587563
Ramiro Sosa <ramiro.sosa@gmail.com>
2019-02-19 23:32:12 -0300

    Part 3 - Getting Real with an API: Axios instead of Fetch

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
+import axios from 'axios';
 
 const DEFAULT_QUERY = "redux";
 const DEFAULT_HPP = "100";
@@ -121,11 +122,10 @@ class App extends Component {
   }
 
   fetchSearchTopStories(searchTerm, page = 0) {
-    fetch(
+    axios(
       `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`
     )
-      .then(response => response.json())
-      .then(result => this.setSearchTopStories(result))
+      .then(result => this.setSearchTopStories(result.data))
       .catch(error => this.setState({ error }));
   }
 
