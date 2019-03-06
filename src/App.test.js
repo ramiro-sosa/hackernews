0a3de1818e0e821791bc9d2fb84c3b53730dc040
Ramiro Sosa <ramiro.sosa@gmail.com>
2019-03-06 09:50:58 -0300

    Part 4 - Code Organization and Testing: Snapshot Tests with Jest (snapshot for Search component)

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import renderer from "react-test-renderer";
-import App from "./App";
+import App, { Search } from "./App";
 
 describe("App", () => {
 
@@ -14,6 +14,24 @@ describe("App", () => {
   test('has a velid snapshot', () => {
     const component = renderer.create(
       <App />
+    )
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+  
+});
+
+describe("Search", () => {
+
+  it("renders without crashing", () => {
+    const div = document.createElement("div");
+    ReactDOM.render(<Search>Search</Search>, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  test('has a velid snapshot', () => {
+    const component = renderer.create(
+      <Search>Search</Search>
     )
     let tree = component.toJSON();
     expect(tree).toMatchSnapshot();
