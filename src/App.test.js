b90965ff97cbfc4cbdfca9ca1fdd44c7a167076d
Ramiro Sosa <ramiro.sosa@gmail.com>
2019-03-06 10:18:37 -0300

    Part 4 - Code Organization and Testing: Snapshot Tests with Jest (snapshot for Table component)

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import renderer from "react-test-renderer";
-import App, { Search, Button } from "./App";
+import App, { Search, Button, Table } from "./App";
 
 describe("App", () => {
 
@@ -50,6 +50,31 @@ describe("Button", () => {
   test('has a valid snapshot', () => {
     const component = renderer.create(
       <Button>Give Me More</Button>
+    )
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+  
+});
+
+describe("Table", () => {
+
+  const props = {
+    list: [
+      { title: '1', author: '1', num_comments: 1, points: 2, objectID: 'y'},
+      { title: '2', author: '2', num_comments: 1, points: 2, objectID: 'z'},
+    ],
+  };
+
+  it("renders without crashing", () => {
+    const div = document.createElement("div");
+    ReactDOM.render(<Table { ...props} />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  test('has a valid snapshot', () => {
+    const component = renderer.create(
+      <Table {...props} />
     )
     let tree = component.toJSON();
     expect(tree).toMatchSnapshot();
